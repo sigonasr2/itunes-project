d45526f2d41872f9604e933883c990edc19ae4a4
Joshua Sigona <sigonasr2@gmail.com>
2020-06-24 04:40:47 +0900

    Add feature-movies, implement step 4.

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var data = require('./data.json').results
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get("/",(req,res)=>{
-	var kind="song"
+app.get("/:kind",(req,res)=>{
+	var kind=req.params.kind
 	if (req.query.albumName) {
 		var albumName = req.query.albumName;
 		var filteredSongs = data.filter((song)=>song.kind===kind&&albumName===song.collectionName);
@@ -32,34 +32,8 @@ app.get("/",(req,res)=>{
 	}
 })
 
-app.get("/video",(req,res)=>{
-	var kind="music-video"
-	if (req.query.albumName) {
-		var albumName = req.query.albumName;
-		var filteredSongs = data.filter((song)=>song.kind===kind&&albumName===song.collectionName);
-		res.send(JSON.stringify(filteredSongs))
-	} else 
-	if (req.query.albumId) {
-		var albumId = Number(req.query.albumId);
-		var filteredSongs = data.filter((song)=>song.kind===kind&&albumId===song.collectionId);
-		res.send(JSON.stringify(filteredSongs))
-	} else 
-	if (req.query.trackName) {
-		var trackName = req.query.trackName;
-		var filteredSongs = data.filter((song)=>song.kind===kind&&trackName===song.trackName);
-		res.send(JSON.stringify(filteredSongs))
-	} else 
-	if (req.query.id) {
-		var searchId = Number(req.query.id);
-		var filteredSongs = data.filter((song)=>song.kind===kind&&searchId===song.trackId);
-		res.send(JSON.stringify(filteredSongs))
-	} else {
-		res.send(JSON.stringify(data.filter((song)=>song.kind===kind)))
-	}
-})
-
-app.post("/update",(req,res)=>{
-	var kind="song"
+app.post("/:kind/update",(req,res)=>{
+	var kind=req.params.kind
 	if (req.body && req.body.songId) {
 		var songId = Number(req.body.songId);
 		var filteredSongs = data.filter((song)=>song.kind===kind&&songId===song.trackId);
@@ -70,32 +44,14 @@ app.post("/update",(req,res)=>{
 				}
 			}
 		}))
-		res.send("Updated "+filteredSongs.length+" song(s)")
+		res.send("Updated "+filteredSongs.length+" "+kind+"(s)")
 	} else {
 		res.status(400).send("songId not specified.");
 	}
 })
 
-app.post("/video/update",(req,res)=>{
-	var kind="music-video"
-	if (req.body && req.body.songId) {
-		var songId = Number(req.body.songId);
-		var filteredSongs = data.filter((song)=>song.kind===kind&&songId===song.trackId);
-		res.send(filteredSongs.forEach((song)=>{
-			for (var key in req.body) {
-				if (key !== "songId") {
-					song[key] = req.body[key]
-				}
-			}
-		}))
-		res.send("Updated "+filteredSongs.length+" video(s)")
-	} else {
-		res.status(400).send("songId not specified.");
-	}
-})
-
-app.post("/delete",(req,res)=>{
-	var kind="song"
+app.post("/:kind/delete",(req,res)=>{
+	var kind=req.params.kind
 	if (req.body && req.body.songId) {
 		var songId = Number(req.body.songId);
 		var filteredSongs = data.filter((song)=>song.kind===kind&&songId===song.trackId);
@@ -110,24 +66,8 @@ app.post("/delete",(req,res)=>{
 	}
 })
 
-app.post("/video/delete",(req,res)=>{
-	var kind="music-video"
-	if (req.body && req.body.songId) {
-		var songId = Number(req.body.songId);
-		var filteredSongs = data.filter((song)=>song.kind===kind&&songId===song.trackId);
-		for (var i=0;i<data.length;i++) {
-			if (filteredSongs.includes(data[i])) {
-				data.splice(i,1)
-			}
-		}
-		res.send("Removed "+filteredSongs.length+" video(s)")
-	} else {
-		res.status(400).send("songId not specified.");
-	}
-})
-
-app.get("/artist",(req,res)=>{
-	var kind="song"
+app.get("/:kind/artist",(req,res)=>{
+	var kind=req.params.kind
 	if (req.query.id) {
 		var artistId = Number(req.query.id);
 		var filteredSongs = data.filter((song)=>song.kind===kind&&artistId===song.artistId);
@@ -147,11 +87,11 @@ function SongIdDoesNotExist(trackId) {
 	return data.filter((song)=>song.kind===kind&&trackId===song.trackId).length===0;
 }
 
-app.post("/add",(req,res)=>{
-	var kind="song"
+app.post("/:kind/add",(req,res)=>{
+	var kind=req.params.kind
 	if (req.body && req.body.trackName
 	 && req.body.trackId  && req.body.collectionName && req.body.collectionId
-	 && req.body.artistId && req.body.artistName) {
+	 && req.body.artistId && req.body.artistName && (kind!=="music-video" || (kind==="music-video" && req.body.previewUrl)) && (kind!=="feature-movie" || (kind==="feature-movie" && req.body.previewUrl && req.body.longDescription))) {
 		 if (SongIdDoesNotExist(req.body.trackId)) {
 			 var song = {}
 			 for (var key in req.body) {
@@ -159,33 +99,12 @@ app.post("/add",(req,res)=>{
 			}
 			song["kind"] = kind;
 			data.push(song)
-			res.send("Added song "+JSON.stringify(song));
+			res.send("Added "+kind+" "+JSON.stringify(song));
 		 } else {
 			res.status(400).send("songId already exists.");
 		 }
 	} else {
-		res.status(400).send("trackId / trackName / collectionName / collectionId / artistId / artistName not specified.");
-	}
-})
-
-app.post("/video/add",(req,res)=>{
-	var kind="music-video"
-	if (req.body && req.body.trackName
-	 && req.body.trackId  && req.body.collectionName && req.body.collectionId
-	 && req.body.artistId && req.body.artistName && req.body.previewUrl) {
-		 if (SongIdDoesNotExist(req.body.trackId)) {
-			 var song = {}
-			 for (var key in req.body) {
-				song[key] = req.body[key]
-			}
-			song["kind"] = kind;
-			data.push(song)
-			res.send("Added video "+JSON.stringify(song));
-		 } else {
-			res.status(400).send("songId already exists.");
-		 }
-	} else {
-		res.status(400).send("trackId / trackName / collectionName / collectionId / artistId / artistName / previewUrl not specified.");
+		res.status(400).send("trackId / trackName / collectionName / collectionId / artistId / artistName "+(kind==="music-video"?"/ previewUrl":"")+""+(kind==="feature-movie"?"/ previewUrl / longDescription":"")+" not specified.");
 	}
 })
 
